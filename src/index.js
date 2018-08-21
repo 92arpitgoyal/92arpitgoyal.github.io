da7b46edc511fd90cee4af38f27e02ad585de1c5
Arpit Goyal <arpit.goyal@decurtis.com>
2018-08-22 02:58:46 +0530

    add home and work illustrations

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import "@babel/polyfill";
 import "styles/main.scss";
 import "assets/images/arpit-goyal.png";
 import "assets/images/share.jpg";
+
+import "assets/images/home/code.png";
+import "assets/images/home/write.png";
+import "assets/images/home/everything.png";
+
+import "assets/images/work/boxmyspace.jpg";
+import "assets/images/work/explara.jpg";
+import "assets/images/work/gojavas.jpg";
+import "assets/images/work/parallellabs.jpg";
+import "assets/images/work/thankyouindia.jpg";
+import "assets/images/work/townscript.jpg";
+import "assets/images/work/ufaber.jpg";
+import "assets/images/work/webengage.jpg";
